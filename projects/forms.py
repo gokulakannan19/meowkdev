5aca0b2ded5e231c9649f4d9c174868ade681ef8
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-10-08 20:18:58 +0530

    theme installed

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,6 @@
-from django.forms import ModelForm
+from django import forms
+from django.forms import ModelForm, widgets
+from django import forms
 from .models import Project
 
 
@@ -7,3 +9,16 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'featured_image', 'description',
                   'demo_link', 'source_link', 'tags']
+
+        widgets = {
+            'tags': widgets.CheckboxSelectMultiple()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update(
+                {'class': 'input', })
+
+        # self.fields['title'].widget.attrs.update({'class': 'input'})
