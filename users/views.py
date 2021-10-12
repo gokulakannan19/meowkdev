0003d495f91cf0b04fefe78592a953bff95a52df
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-10-12 12:08:00 +0530

    user authentication

--- users/views.py
+++ users/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+from django.contrib.auth.models import User
 from .models import Profile
+
 # Create your views here.
+
+
+def login_user(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Username does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profiles')
+        else:
+            messages.error(request, "You entered wrong password")
+
+    return render(request, 'users/login-register.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.error(request, "User was successfully logged out")
+    return redirect('login')
 
 
 def profiles(request):
