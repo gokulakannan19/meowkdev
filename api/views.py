782910f9c4bcaa3ce7e47f89737cadcfa119a738
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-10-20 12:33:36 +0530

    voted through postman

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from projects.models import Project
+from projects.models import Project, Review
 from .serializers import ProjectSerializer
 
 # from django.http import JsonResponse
@@ -23,7 +23,6 @@ def get_routes(request):
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
 def get_projects(request):
     projects = Project.objects.all()
     serializer = ProjectSerializer(projects, many=True)
@@ -32,9 +31,31 @@ def get_projects(request):
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
 def get_project(request, pk):
     project = Project.objects.get(id=pk)
     serializer = ProjectSerializer(project, many=False)
     # print(serializer)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def project_vote(request, pk):
+    project = Project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    print("DATA:", data)
+
+    review, created = Review.objects.get_or_create(
+        owner=user,
+        project=project,
+    )
+
+    review.value = data['value']
+    review.save()
+    print("sucessfully saved")
+    project.get_vote_ratio
+
+    serializer = ProjectSerializer(project, many=False)
     return Response(serializer.data)
