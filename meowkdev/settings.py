896878d59d0a57b71d80356d64acc520d076f6f6
gokulakannan19 <66069114+gokulakannan19@users.noreply.github.com>
2021-10-23 13:42:02 +0530

    order changed for static files

--- meowkdev/settings.py
+++ meowkdev/settings.py
@@ -193,15 +193,19 @@ EMAIL_HOST_PASSWORD = "kumgcecctipfiygo"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
